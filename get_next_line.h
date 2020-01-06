dde8f7c56e981f9cc19fce307cb5eac91f35b733
iNathanZ <inathan.zrb@gmail.com>
2020-01-06 15:44:48 +0100

    first push, hope its last

---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_getnextline_2019
+** File description:
+** get_next_line.h
+*/
+
+#ifndef READ_SIZE
+#define READ_SIZE 16
+#endif
+
+char *get_next_line(int fd);
